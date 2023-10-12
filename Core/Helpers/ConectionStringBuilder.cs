c0b1e9e8077fb935d50d3bf0009c02f1b8ec479f
rewqla <desares526@gmail.com>
2023-10-12 17:00:49 +0300

    Update ConectionStringBuilder with enviroments

--- Core/Helpers/ConectionStringBuilder.cs
+++ Core/Helpers/ConectionStringBuilder.cs
@@ -10,17 +10,33 @@ namespace Core.Helpers
 {
     public static class ConnectionStringBuilder
     {
-        public static IConfiguration Configuration { get; set; }
-        public static string GenerateConnectionString(IConfiguration? configuration=null)
+        //public static IConfiguration Configuration { get; set; }
+        //public static string GenerateConnectionString(IConfiguration? configuration = null)
+        //{
+        //    if (configuration != null)
+        //        Configuration = configuration;
+
+        //    var dbSettings = Configuration.GetSection("DbSecrets").Get<DbConnectionSettings>();
+
+        //    string connectionString = $"Server={dbSettings.DbHost};Port=3306;Database={dbSettings.DbName};User Id={dbSettings.DbUser};Password={dbSettings.DbPassword};Connect Timeout=30;SslMode=None";
+
+        //    return connectionString;
+
+        //}
+        public static string GenerateConnectionString()
         {
-            if(configuration!=null)
-                Configuration = configuration;
+            var dbHost = Environment.GetEnvironmentVariable("DbHost");
+            var dbUser = Environment.GetEnvironmentVariable("DbUsername");
+            var dbPass = Environment.GetEnvironmentVariable("DbPassword");
+            var dbName = Environment.GetEnvironmentVariable("DbName");
 
-            var dbSettings = Configuration.GetSection("DbSecrets").Get<DbConnectionSettings>();
+            if (dbHost == null && dbUser == null && dbPass == null && dbName == null)
+                return "";
 
-            string connectionString = $"Server={dbSettings.DbHost};Port=3306;Database={dbSettings.DbName};User Id={dbSettings.DbUser};Password={dbSettings.DbPassword};Connect Timeout=30;SslMode=None";
+            string connectionString = $"Server={dbHost};Port=3306;Database={dbName};User Id={dbUser};Password={dbPass};Connect Timeout=30;SslMode=None";
 
             return connectionString;
+
         }
     }
     public class DbConnectionSettings
